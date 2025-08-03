f5a07843e339ab39e1e4da290f932a83fee81686
Macintosh HD <macintoshhd@macbooks-som.local>
2025-08-04 01:00:20 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useContext } from "react"
+import Display from "./components/display"
+import Player from "./components/Player"
+import Sidebar from "./components/sidebar"
+import { PlayerContext } from "./context/PlayerContext"
+
+const App = () => {
+  const {audioRef,track} = useContext(PlayerContext)
+  return (
+    <div className="h-screen bg-black">
+      <div className="h-[90%] flex">
+        <Sidebar/>
+        <Display/>
+      </div>
+      <Player/>
+      <audio ref={audioRef} src={track.file} preload="auto">
+
+      </audio>
+    </div>
+  )
+}
+
+export default App
